1350b026b4d9666c64ef70accd39249ef472ec26
sotiriskaragiannis <skaragian03@gmail.com>
2025-04-05 13:21:57 +0300

    stdin mode also on no args

--- sk-cat.py
+++ sk-cat.py
@@ -5,36 +5,47 @@ import argparse
 
 READ_FROM_STDIN_STRING = "-"
 
-# Initialise the parser
-parser = argparse.ArgumentParser("concatenate and print files")
-parser.add_argument("filenames", nargs='+', help="filenames of the files to concatenate and print", type=str)
-args = parser.parse_args()
-
-
-for filename in args.filenames:
-
-    if filename != READ_FROM_STDIN_STRING:
+def stdinMode():
+    while True:
         try:
-            with open(filename) as file:
-                print(file.read(), end="")
-
-        except FileNotFoundError:
-            print(f"file {filename} was not found")
-
-        except IsADirectoryError:
-            print(f"{filename} is a directory and not a file")
-
+            print(input(""))
+        except KeyboardInterrupt:
+            print("")
+            exit()
         except:
-            print("An exception occurred")
+            break
 
-    # Read from stdin
+def filenameMode():
+    try:
+        with open(filename) as file:
+            print(file.read(), end="")
+
+    except FileNotFoundError:
+        print(f"file {filename} was not found")
+
+    except IsADirectoryError:
+        print(f"{filename} is a directory and not a file")
+
+    except:
+        print("An exception occurred")
+
+
+def main(args=None):
+    # Initialise the parser
+    parser = argparse.ArgumentParser("concatenate and print files")
+    parser.add_argument("filenames", nargs='*', help="filenames of the files to concatenate and print", type=str)
+    args = parser.parse_args()
+
+    # if zero filename args where provided then go to stdin mode
+    if not args.filenames:
+        stdinMode()
     else:
-        # print("stdin reading...")
-        while True:
-            try:
-                print(input(""))
-            except KeyboardInterrupt:
-                print("")
-                exit()
-            except:
-                break
+        for filename in args.filenames:
+            if filename != READ_FROM_STDIN_STRING:
+                filenameMode()
+            else:
+                stdinMode()
+
+
+if __name__ == "__main__":
+    main()
